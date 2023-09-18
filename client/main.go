1c52f79d9b62c40231214cc2a2822cd3daa5f1f9
Emelit0 <stollk811@gmail.com>
2023-09-18 14:59:13 +0200

    init client

---
+++ client/main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+   Listen for incoming http requests from the clients
+*/
+
+func listenHttp() {
+	var request string
+
+	// Listen for incoming http requests from the clients
+	// and send them to the controller
+
+}
