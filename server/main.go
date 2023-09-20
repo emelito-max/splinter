51b669cd5d0898a0b7c0ce24ea23e3cf7dd5786c
Emelit0 <stollk811@gmail.com>
2023-09-20 20:22:52 +0200

    init server

--- server/main.go
+++ server/main.go
@@ -12,6 +12,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
+	// switch to https
 	http.ListenAndServe(":8080", nil)
 
 }
